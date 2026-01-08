6a292e7c7d84bda1b7f716b8726ea34d827e21bb
gabrielaguedes12 <gabr1.profissional@gmail.com>
2026-01-07 22:45:56 -0300

    nova estilizacao hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,32 +1,31 @@
 import './Hero.scss';
 import imagem from '../../img/perfilGabi.png';
 
-export default function Hero(){
+export default function Hero() {
 
-    return(
+    return (
         <>
-        <section className='hero'>
-            <div className='perfil'>
-            <h1 className='nomePerfil'>Gabriela Guedes</h1>
-            <img className='perfilGabi' src={imagem} alt="Gabriela Guedes" />
-            <p className='descricaoPerfil'>Para você me conhecer melhor, é necessário saber que carrego dois olhares complementares: 
-                o profundo da <strong>Fotografia</strong> e o analítico da <strong>Programação</strong>.A fotografia me ensinou a observar detalhes,
-                 contextos e histórias; a programação, a estruturar ideias, resolver problemas e transformar conceitos em soluções digitais.</p>
-            </div>
-        </section>
+            <section className='hero'>
+                <div className='perfil'>
+                    <h1 className='nomePerfil'>Desenvolvedora Full-Stack</h1> 
+                    <h1 className='nomePerfil'>Gabriela Guedes</h1>
 
-        <section className='hero_imagem'>
-        <div className='imgMaior'>
-            <img className='imgProgramacao' src="https://images.pexels.com/photos/16592498/pexels-photo-16592498.jpeg" alt="Programação" />
-        </div>
+                    <img className='perfilGabi' src={imagem} alt="Gabriela Guedes" />
 
-        <button>Currículo</button>
-        <button>GitHub</button>
+                    <p className='descricaoPerfil'>Para você me conhecer melhor, é necessário saber que carrego dois olhares complementares:
+                        o profundo da <strong>Fotografia</strong> e o analítico da <strong>Programação</strong>. A fotografia me ensinou a observar detalhes,
+                        contextos e histórias; a programação, a estruturar ideias, resolver problemas e transformar conceitos em soluções digitais.
+                    </p>
+                </div>
 
-        <div className='imgMenor'>
-            <img className='imgFotografia' src="https://images.pexels.com/photos/442573/pexels-photo-442573.jpeg" alt="Fotografia" />
-        </div>
-        </section>
+            
+            <section className='hero_button'>
+                <div>
+                    <button>Currículo</button>
+                    <button>GitHub</button>
+                </div>
+            </section>
+            </section>
         </>
     );
 }
